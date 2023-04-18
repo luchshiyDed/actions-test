ff6f0ba7b59f525f36fc4a91dc8f51f402d96f5a
luchshiyDed <81702687+luchshiyDed@users.noreply.github.com>
2023-04-18 18:06:42 +0700

    ttt

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='test',
+      version='0.0',
+      description='test',
+      packages=['action'],
+      author_email='09090legX@gmail.com',
+      zip_safe=False)
