a9d759618ec151510efc0dc3143289e97ba85117
luchshiyDed <81702687+luchshiyDed@users.noreply.github.com>
2023-04-30 14:23:01 +0700

    t

--- setup1.py
+++ setup1.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='testingGithubActionsForPip1',
-      version='0.0',
+      version='0.1',
       description='test',
       packages=['action'],
       author_email='09090legX@gmail.com',
